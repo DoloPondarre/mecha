d70bce0041c75d2bb01262cc8dd77f237ade1b7b
Dolores Pondarré <dolopondarre@icloud.com>
2024-05-13 18:20:02 +0200

    Validaciones en Products, y en el _form de admin/products

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,22 @@ class Product < ApplicationRecord
 
     attribute :remove_images, :boolean, default: false
 
+    validates :name, :price, :description, :color, :size, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
+    validate :validate_images
+
+    private
+
+    def validate_images
+        if images.attached?
+        images.each do |image|
+            if !image.content_type.in?(%('image/jpeg image/png'))
+            errors.add(:images, 'debe ser un archivo JPEG o PNG')
+            end
+        end
+        else
+        errors.add(:images, 'debes adjuntar al menos una imagen')
+        end
+    end
+
 end
